0db5ad51aabe4f29b5af84da5871c6691bb87fe9
showkhin1997 <showkhin15@gmail.com>
2023-05-29 21:02:35 +0600

    new item add by button event handeler

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 // top player header
 document.getElementById('top-player').style.color = "rgba(0, 148, 93, 1)";
+
+// single player background style
+const elemnts = document.getElementsByClassName('single-player-background');
+for (const elemnt of elemnts) {
+    elemnt.style.backgroundColor = 'rgba(0, 169, 58, 1)';
+}
+
+
+// new item add
+document.getElementById('add-item-button').addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = 'New Item';
+    const addItem = document.getElementById('item-add');
+    addItem.appendChild(li);
+})
